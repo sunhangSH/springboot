58ed96e290d6fb7fef7ce3a486fbb08da5987273
sunhang <xkwz@163.com>
2021-11-22 00:20:18 +0800

    2-8 增加配置项实例

--- src/main/java/com/meteorxsh/wiki/controller/TestController.java
+++ src/main/java/com/meteorxsh/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.meteorxsh.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
     //RequestMapping支持所有的请求方式
 //    @RequestMapping(value = "/user/1",method = RequestMethod.DELETE)
 //    @RequestMapping(value = "/user/1",method = RequestMethod.GET)
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
