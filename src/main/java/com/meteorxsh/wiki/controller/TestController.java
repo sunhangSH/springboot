da9e43cc54c479637f5792e72f7dd66e5554004b
sunhang <xkwz@163.com>
2021-11-21 23:58:20 +0800

    2-7 增加hello测试接口 test.http

---
+++ src/main/java/com/meteorxsh/wiki/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.meteorxsh.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shMeteor
+ * @create 2021-11-17-23:23
+ */
+
+/**
+ * GET,POST,PUT,DELETE
+ *
+ * /user?id=1
+ * /user/1
+ */
+@RestController
+public class TestController {
+    //RequestMapping支持所有的请求方式
+//    @RequestMapping(value = "/user/1",method = RequestMethod.DELETE)
+//    @RequestMapping(value = "/user/1",method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post," + name;
+    }
+}
