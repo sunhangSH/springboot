24346b336f814f3d9204fe6709eac40f8b9a25f4
sunhang <xkwz@163.com>
2021-11-28 20:34:50 +0800

    3-6 增加通用返回类 CommonResp

--- src/main/java/com/meteorxsh/wiki/controller/EbookController.java
+++ src/main/java/com/meteorxsh/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.meteorxsh.wiki.controller;
 
 import com.meteorxsh.wiki.domain.Ebook;
+import com.meteorxsh.wiki.resp.CommonResp;
 import com.meteorxsh.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
---
+++ src/main/java/com/meteorxsh/wiki/resp/CommonResp.java
@@ -0,0 +1,46 @@
+package com.meteorxsh.wiki.resp;
+
+/**
+ * @author shMeteor
+ * @create 2021-11-28-20:21
+ */
+public class CommonResp<T> {
+    /**
+     * 业务上的成功或失败
+     */
+    private boolean success = true;
+
+    /**
+     * 返回信息
+     */
+    private String message;
+
+    /**
+    返回范型数据，自定义类型
+     */
+    private T content;
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+}
