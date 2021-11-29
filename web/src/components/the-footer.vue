9704bb176156c836a0b65b51e862016a71928a0e
sunhang <xkwz@163.com>
2021-11-30 00:16:07 +0800

    4-7 制作the-header，the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,11 @@
+<template><a-layout-footer style="text-align: center">
+  Wiki Created by Meteor
+</a-layout-footer></template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
