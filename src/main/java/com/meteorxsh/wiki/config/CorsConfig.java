8d3ad98cb5b96893d02ffa8f34f19ae42d40c4f1
sunhang <xkwz@163.com>
2021-12-03 00:16:43 +0800

    5-2 使用axios调用后端接口/ebook/list

---
+++ src/main/java/com/meteorxsh/wiki/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.meteorxsh.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author shMeteor
+ * @create 2021-12-03-00:08
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
