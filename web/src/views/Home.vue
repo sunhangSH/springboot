8b93784ce630517eb718559ebcdda1c0a5af576e
sunhang <xkwz@163.com>
2021-12-05 22:47:43 +0800

    5-5 修改axios的baseURL

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -95,7 +95,7 @@ export default defineComponent({
     const ebooks1 = reactive({books:[]});
     onMounted(() => {
       console.log("onMounted")
-      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then(
+      axios.get("/ebook/list").then(
           (response) =>{
             const data = response.data;
             ebooks.value = data.content;
