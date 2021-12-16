d76f74341763c3da37ad4f88324e566e6c373c70
sunhang <xkwz@163.com>
2021-12-17 01:01:59 +0800

    5-9 增加aop，打印接口耗时，请求参数和返回参数。去掉过滤器、拦截器

--- src/main/java/com/meteorxsh/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/meteorxsh/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,24 @@
-package com.meteorxsh.wiki.config;
-
-import com.meteorxsh.wiki.interceptor.LogInterceptor;
-import javax.annotation.Resource;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author shMeteor
- * @create 2021-12-14-23:38
- */
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-  @Resource
-  LogInterceptor logInterceptor;
-
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(logInterceptor)
-        .addPathPatterns("/**").excludePathPatterns("/login");
-  }
-}
+//package com.meteorxsh.wiki.config;
+//
+//import com.meteorxsh.wiki.interceptor.LogInterceptor;
+//import javax.annotation.Resource;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author shMeteor
+// * @create 2021-12-14-23:38
+// */
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//  @Resource
+//  LogInterceptor logInterceptor;
+//
+//  public void addInterceptors(InterceptorRegistry registry) {
+//    registry.addInterceptor(logInterceptor)
+//        .addPathPatterns("/**").excludePathPatterns("/login");
+//  }
+//}
